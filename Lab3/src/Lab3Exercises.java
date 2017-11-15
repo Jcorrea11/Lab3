f4bfd4d001b0c0d51a672c0c15be53df497c2b32
4010a3 <4010a3@china.ece.uprm.edu>
2017-11-15 12:23:12 -0400

    Test

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -2,7 +2,7 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Jose Luis Correa");
+		JFrame myFrame = new JFrame("Jose L. Correa");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myFrame.setLocation(875, 400);
 		myFrame.setSize(200, 200);
